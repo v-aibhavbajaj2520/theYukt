6a984c53cf092c605984907338f72c31a3480021
Vaibhav Bajaj <97230062+v-aibhavbajaj2520@users.noreply.github.com>
2025-02-10 10:10:00 +0530

    initial commit

---
+++ theYukt/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2>hello world!</h2>
+    </>
+  )
+}
+
+export default App
