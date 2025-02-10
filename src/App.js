a8eda071de556b6adcb98176e1a1dcbfa7e0eda2
Vineet Raj <vineetcres@gmail.com>
2025-02-10 18:27:02 +0530

    Modules ready, initial
    Done by Bajaj

--- src/App.js
+++ src/App.js
@@ -22,55 +22,22 @@ function App() {
     setIsNavOpen(!isNavOpen);
   };
 
-  useEffect(() => {
-    const targetDate = new Date("2025-02-28T00:00:00").getTime();
-
-    const interval = setInterval(() => {
-      const now = new Date().getTime();
-      const distance = targetDate - now;
-
-      if (distance < 0) {
-        clearInterval(interval);
-        setCountdown({
-          days: "00",
-          hours: "00",
-          minutes: "00",
-          seconds: "00",
-        });
-      } else {
-        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        const hours = Math.floor(
-          (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-        );
-        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-        setCountdown({
-          days: days.toString().padStart(2, "0"),
-          hours: hours.toString().padStart(2, "0"),
-          minutes: minutes.toString().padStart(2, "0"),
-          seconds: seconds.toString().padStart(2, "0"),
-        });
-      }
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, []);
 
   return (
     <div className="container">
       {/* Navbar */}
       <nav className="navbar">
         <div className="logo">
-          <h2>H25</h2>
+          <img src=''/>
         </div>
         <div className={`nav-links ${isNavOpen ? "active" : ""}`}>
-          <a href="#team">Team</a>
-          <a href="#themes">Themes</a>
-          <a href="#sponsor">Sponsor Us</a>
-          <a href="#about">About</a>
-          <a href="#faq">FAQ</a>
-          <button className="register-btn">REGISTER NOW</button>
+          <a href="#team">Home</a>
+          <a href="#themes">About Us</a>
+          <a href="#sponsor">Service</a>
+          <a href="#about">Careers</a>
+        </div>
+        <div>
+          <button className="register-btn">Get a Free Quote</button>
         </div>
         <button className="nav__button" onClick={handleNavToggle}>
           <img
@@ -84,67 +51,50 @@ function App() {
 
       {/* Main Content */}
       <main className="main-content">
-        {/* Hero Section */}
-        <div className="hero-section">
-          <div className="hero-image"></div>
           <div className="hero-content">
-            <h1>Hackathon 2025</h1>
-            <p className="date">February 28, 2025 — March 1, 2025</p>
-            <h3>Code - Compete - Conquer</h3>
-            <p className="description">
-              Tech for Good: Empowering communities for a Better Tomorrow
-            </p>
-            <div className="countdown-container">
-              <h2 className="countdown-title">Live in</h2>
-              <div className="countdown-wrapper">
-                <div className="countdown-box">
-                  <div className="countdown-value">{countdown.days}</div>
-                  <div className="countdown-label">days</div>
-                </div>
-                <div className="countdown-box">
-                  <div className="countdown-value">{countdown.hours}</div>
-                  <div className="countdown-label">hours</div>
-                </div>
-                <div className="countdown-box">
-                  <div className="countdown-value">{countdown.minutes}</div>
-                  <div className="countdown-label">mins</div>
-                </div>
-                <div className="countdown-box">
-                  <div className="countdown-value">{countdown.seconds}</div>
-                  <div className="countdown-label">secs</div>
-                </div>
+            <h1>"Yukt- Many minds, one masterpiece"</h1>
+        </div>
+
+        
+      </main>
+      <div className="about-us">
+        <h1 className="about-title">About Us</h1>
+        <section className="about-content">
+        <p>Yukt delivers web & app development,<br/> UI/UX design, video editing, and<br/> software solutions with innovation and<br/> precision.<br/>
+        “Your Vision, Our Team"</p>
+        <iframe src="youtube.com"></iframe>
+        </section>
+      </div>
+
+      <div className="expertise">
+        <h1 className="expertise-title">Our Expertise</h1>
+        <section>
+          <div className="expertise">
+            <div className="expertise-content">
+              <div className="mid-logo">
+                <img src="logo.jpeg"></img>
               </div>
+
+                <div className="circles circle-1">
+                  <h3>UI/UX<br/>Designing</h3>
+                </div>
+                <div className="circles circle-2">
+                  <h3>Web<br/>Development</h3>
+                </div>
+                <div className="circles circle-3">
+                  <h3>App <br/>Development</h3>
+                </div>
+                <div className="circles circle-4">
+                  <h3>Graphic<br/>Designing</h3>
+                </div>
+
             </div>
-            <button className="register-btn">REGISTER</button>
+            
           </div>
-        </div>
-
-        {/* Stats Section */}
-        <StatsSection />
-
-        {/* Sponsor Section */}
-        <Sponsor heading={"Sponsors"} dataArray={SPONSOR} />
-
-        {/* Why Participate Section */}
-        <div className="how-it-works">
-          <h2>Why Participate in a Hackathon?</h2>
-          <p>
-            Unleash your creativity, build innovative projects, and collaborate
-            with like-minded problem solvers. Whether you're a beginner or a
-            pro, hackathons give you the chance to innovate by turning ideas
-            into reality, learn from cutting-edge tech and expert mentors,
-            network with developers, designers, and industry leaders, and win
-            exciting prizes and recognition. Ready to build and innovate?
-            Register now and make an impact.
-          </p>
-        </div>
-
-        {/* Event Container Section */}
-        <EventContainer heading={"Our Theme"} dataArray={EVENTS} />
-
-        {/* Timeline Section */}
-        <div className="timeline-section">
-          <h2>Timeline</h2>
+        </section>
+      </div>
+      <div className="timeline-section">
+          <h1>What Sets Us Apart</h1>
           <div className="timeline">
             <div className="timeline-item">
               <div className="timeline-date">28 February 2025</div>
@@ -170,10 +120,104 @@ function App() {
                 </p>
               </div>
             </div>
+            <div className="timeline-item">
+              <div className="timeline-date">28 February 2025</div>
+              <div className="timeline-content">
+                <h3 className="right-timeline">Track Release</h3>
+                <p className="right-timeline">
+                  Get ready to innovate! On January 1, we’ll unveil the
+                  hackathon tracks, each designed to spark creativity and tackle
+                  real-world challenges. Gear up to choose your path and make a
+                  difference!
+                </p>
+              </div>
+            </div>
+            <div className="timeline-item">
+              <div className="timeline-date">1 March 2025</div>
+              <div className="timeline-content">
+                <h3 className="left-timeline">Rolling Acceptance Idea Submission</h3>
+                <p className="left-timeline">
+                  Submit your innovative ideas early, as we have limited seats!
+                  We’re reviewing submissions as they come in, and you’ll be
+                  notified about your selection status within 36 hours. Don’t
+                  miss this opportunity!
+                </p>
+              </div>
+            </div>
           </div>
+        </div> 
+        <div className="contact-container">
+          <div className="b-card">
+            <div className="input-det">
+              <label>First Name</label>
+              <input type="text"></input>
+            </div>
+
+            <div className="input-det">
+              <label>Last Name</label>
+              <input type="text"></input>
+            </div>
+
+            <div className="input-det">
+              <label>Email</label>
+              <input type="text"></input>
+            </div>
+
+            <div className="input-det">
+              <label>Phone Number</label>
+              <input type="text"></input>
+            </div>
+
+            <div className="text-det">
+              <label>Write your message</label>
+              <textarea type="text"></textarea>
+            </div>
+
+            <div className="send-message">
+            <button type="submit">Send Message</button>
+            </div>
+          </div>
+          <div className="s-card">
+            <h2>Contact Information</h2>
+            <h3>Say something to start a chat</h3>
+            <img src="aa"></img><h4>+91 8486964047</h4>
+            <img src="aa"></img><h4>info.vbajaj@gmail.com</h4>
+            <img src="aa"></img><h4>Delhi</h4>
+
+            <img src="aa"></img>
+            <img src="aa"></img>
+            <img src="aa"></img>
+              </div>
+          
         </div>
-      </main>
+      
+      <hr className="hori-line"></hr>
+      <footer>
+        <div className="left-side">
+        <h3>Reach Us</h3>
+        <img src="aa"></img><h4>+91 8486964047</h4>
+            <img src="aa"></img><h4>info.vbajaj@gmail.com</h4>
+            <img src="aa"></img><h4>Delhi</h4>
+        </div>
+
+        <div className="mid-side">
+        <h3>Company</h3>
+          <h4>About</h4>
+          <h4>Contact Us</h4>
+          <h4>Careers</h4>
+        </div>
+
+        <div className="right-side">
+        <h3>Quick Links</h3>
+          <a><h4>InstaGram</h4></a>
+          <a><h4>Linkedin</h4></a>
+          <a><h4>Twitter</h4></a>
+        </div>
+        
+      </footer>
+
     </div>
+    
   );
 }
 
