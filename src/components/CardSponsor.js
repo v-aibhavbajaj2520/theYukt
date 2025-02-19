ab742d867d0a6829ea782e3c359a7d733088fc69
Vaibhav Bajaj <97230062+v-aibhavbajaj2520@users.noreply.github.com>
2025-02-20 02:45:37 +0530

    Revert "deleted"
    This reverts commit 43de4d066a29ddc9e9304235f089f1dc4523ec25.

---
+++ src/components/CardSponsor.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const CardSponsor = ({ index, backgroundImage, title, date, venue, time, buttonText }) => {
+  return (
+    <div
+      className={`event event-${index + 1}`}
+      key={index}
+      style={{ backgroundImage: backgroundImage }}
+    >
+    </div>
+  );
+};
+
+export default CardSponsor;
---
+++ src/config/constants.js
@@ -0,0 +1,107 @@
+export const EVENTS = [
+    {
+      backgroundImage: "url('goals/1.png')"
+    },
+    {
+      backgroundImage: "url('goals/2.png')"
+    },
+    {
+      backgroundImage: "url('goals/3.png')"
+    },
+    {
+      backgroundImage: "url('goals/4.png')"
+    },
+    {
+      
+    backgroundImage: "url('goals/5.png')"
+    },
+    {
+      
+      backgroundImage: "url('goals/6.png')"
+      },
+      {
+      
+        backgroundImage: "url('goals/7.png')"
+        },
+        {
+      
+          backgroundImage: "url('goals/8.png')"
+          },
+          {
+      
+            backgroundImage: "url('goals/9.png')"
+            },{
+      
+              backgroundImage: "url('goals/10.png')"
+              },
+              {
+      
+                backgroundImage: "url('goals/11.png')"
+                },
+                {
+      
+                  backgroundImage: "url('goals/12.png')"
+                  },
+                  {
+      
+                    backgroundImage: "url('goals/13.png')"
+                    },
+                    {
+      
+                      backgroundImage: "url('goals/14.png')"
+                      },
+                      {
+      
+                        backgroundImage: "url('goals/15.png')"
+                        },
+                        {
+      
+                          backgroundImage: "url('goals/16.png')"
+                          },
+                          {
+      
+                            backgroundImage: "url('goals/17.png')"
+                            },
+    
+    
+  ]
+
+  export const SPONSOR = [
+    {  
+      backgroundImage: "url('')"
+      },
+      {  
+        backgroundImage: "url('')"
+        },
+        {  
+          backgroundImage: "url('')"
+          },
+          {  
+            backgroundImage: "url('')"
+            },{  
+              backgroundImage: "url('')"
+              },  
+              {  
+                backgroundImage: "url('')"
+                },
+                {  
+                  backgroundImage: "url('')"
+                  },
+                  {  
+                    backgroundImage: "url('')"
+                    },
+                    {  
+                      backgroundImage: "url('')"
+                      },
+                      {  
+                        backgroundImage: "url('')"
+                        },
+                        {  
+                          backgroundImage: "url('')"
+                          },
+                          {  
+                            backgroundImage: "url('')"
+                            },
+                 
+                  
+  ]
